3b5e6c78f23fe680a52dcd61d6c4a1142c7a96c5
Aman Babu Gautam <amanbabugautam@gmail.com>
2020-09-20 14:22:14 +0530

    updated uiserver

--- microservice_2/uiserver.js
+++ microservice_2/uiserver.js
@@ -10,23 +10,59 @@ app.use(bodyParser.json());
 
 
 const URL = `http://localhost://${port}`
+const DATABASE_SERVICE = `http://localhost:8083`
 
 app.get('/alarms', (req, res) => {
     console.log('Returning alarms list');
-    const alarms = res.body.alarms;
-    res.send(alarms);
+    request.get({
+        headers: {
+            "content-type": "application/json"
+        },
+        url: `${DATABASE_SERVICE}/alarms`
+    }, (err, response, body) => {
+        if (!err) {
+
+            res.status(200).send({ message: `Get all Alarms list`, data: res.body.alarms });
+        } else {
+            res.status(400).send({ problem: `Alarms with issue ${err}` });
+        }
+    })
 });
 
 app.get('/alarms/:starttime/:endtime/:alarmtype', (req, res) => {
     console.log('Returning alarms within given start time, end time and alarm type');
-    const alarms = res.body.alarms;
-    res.send(alarms);
+    
+    request.get({
+        headers: {
+            "content-type": "application/json"
+        },
+        url: `${DATABASE_SERVICE}/alarms`,
+        qs: { starttime: starttime, endtime: endtime, alarmtype: alarmtype }
+    }, (err, response, body) => {
+        if (!err) {
+        	const filteredAlarms = res.body.alarms;
+            res.status(200).send({ message: `Get filtered Alarms`, data: filteredAlarms });
+        } else {
+            res.status(400).send({ problem: `Alarms with issue ${err}` });
+        }
+    })
 });
 
 app.get('/videos', (req, res) => {
     console.log('Returning recorded videos list');
-    const videos = res.body.videos;
-    res.send(videos);
+    request.get({
+        headers: {
+            "content-type": "application/json"
+        },
+        url: `${DATABASE_SERVICE}/videos`
+    }, (err, response, body) => {
+        if (!err) {
+        	const videos = res.body.videos;
+            res.status(200).send({ message: `Get recorded videos`, data: videos });
+        } else {
+            res.status(400).send({ problem: `Videos with issue ${err}` });
+        }
+    })
 });
 
 
